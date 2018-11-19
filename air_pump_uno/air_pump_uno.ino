9ffa40a7076105378919d5ccee2f735f61052dba
alexrussomanno <45177473+alexrussomanno@users.noreply.github.com>
2018-11-19 12:17:09 -0500

    Update air_pump_uno.ino

--- air_pump_uno/air_pump_uno.ino
+++ air_pump_uno/air_pump_uno.ino
@@ -21,7 +21,6 @@ float slope1;
 float int1;
 
 void setup() {
-//  pinMode(regulator,OUTPUT);
   for (int i=0; i < 4; i++){
     pinMode(valve[i], OUTPUT);
   }
@@ -30,8 +29,8 @@ void setup() {
   
   Serial.begin(9600); 
   Serial.println("Let's go!");
-  // Run the following, enter slope and int values below in loop, then comment out this section
-//  pressure_calibration();
+  // Run the following, enter slope and int values below, then comment out "run_pressure_calibration()"
+//  run_pressure_calibration();
   slope1 = 0.0179211483;
   int1 = -1.8817205429;
 }
@@ -84,148 +83,28 @@ void loop() {
   
   
 }
-//  unsigned long prev_time = millis();
-//  unsigned long current_time = millis();
-//  long start_time = current_time;
-//  int val =0;
-//  while (Serial.read() != 10){
-//    while (Serial.available() == 0){
-//      current_time = millis();
-//      int output_volt1 = analogRead(transducer1);
-//      int output_volt2 = analogRead(transducer2);
-//      int output_volt3 = analogRead(transducer3);
-//      //      Serial.print(millis()-start_time);        Serial.print(" ");
-//      //  //      Serial.print((float)i/255*5);  Serial.print(" ");
-//      Serial.print(millis()-start_time);   
-//      Serial.print(" ");
-//      
-//      for (int i=1; i < 8; i++){
-//        Serial.print(digitalRead(valve[i]));
-//      }
-//      Serial.print(digitalRead(valve[8])); 
-//      Serial.print("   ");
-//      Serial.print(abs((float)output_volt1/1023*5.0*slope1+int1)); 
-//      Serial.print(" ");
-//      Serial.print(abs((float)output_volt2/1023*5.0*slope2+int2));  
-//      Serial.print(" ");
-//      Serial.println(abs((float)output_volt3/1023*5.0*slope3+int3));
-////      Serial.print("   ");
-//    }
-//    val = Serial.parseInt();
-//    Serial.read();
-//    if (digitalRead(valve[val])==HIGH){
-//      digitalWrite(valve[val],LOW);
-//    }
-//    else {
-//      digitalWrite(valve[val],HIGH);
-//    }
-////    val = Serial.parseInt();
-//  }
 
-  //  int x = 1;
-  //  for (int i = 0; i > -1; i = i + x){
-  //      analogWrite(regulator,i);
-  //      if (i == max_pressure) x = -1;             // switch direction at peak
-  //      delay(time_step);
-  //      int output_volt1 = analogRead(transducer1);
-  //      int output_volt2 = analogRead(transducer2);
-  //      Serial.print(millis());        Serial.print(" ");
-  ////      Serial.print((float)i/255*5);  Serial.print(" ");
-  //      Serial.print((float)output_volt1/1023*5.0*slope1+int1); Serial.print(" ");
-  //      Serial.println((float)output_volt2/1023*5.0*slope2+int2); 
-  //   } 
-
-
-
-  //  while (Serial.read() != 10);
-  //  int x = 1;
-  //  unsigned long prev_time = millis();
-  //  unsigned long current_time = millis();
-  //  long start_time = current_time;
-  //  int i = 1;
-
-  ////  Electronic Pressure regulator
-  //    while (i > -1){
-  //      current_time = millis();
-  //      if (current_time > prev_time + time_step){
-  //        analogWrite(regulator,i);
-  //        if (i == max_pressure) x = -1;
-  //        i = i + x;
-  //        prev_time = current_time;
-  //      }
-
-
-
-
-
-////    Manual pressure regulator
-//  while (Serial.read() != 10){
-//    current_time = millis();
-//    int output_volt1 = analogRead(transducer1);
-//    int output_volt2 = analogRead(transducer2);
-//    Serial.print(millis()-start_time);        Serial.print(" ");
-//    Serial.print((float)output_volt1/1023*5.0); Serial.print(" ");
-//    Serial.println((float)output_volt2/1023*5.0); 
-//  }
-
-//   int x = 1;
-//   for (int i = 0; i > -1; i = i + x){
-//      analogWrite(regulator,i);
-//      if (i == max_pressure) x = -1;             // switch direction at peak
-//      delay(time_step);
-//      int output_volt1 = analogRead(transducer1);
-//      int output_volt2 = analogRead(transducer2);
-//      Serial.print(millis());        Serial.print(" ");
-////      Serial.print((float)i/255*5);  Serial.print(" ");
-//      Serial.print((float)output_volt1/1023*5.0); Serial.print(" ");
-//      Serial.println((float)output_volt2/1023*5.0); 
-//   } 
-
-
-//  for(int i=0;i<=150;i++){
-//    analogWrite(2,i);
-//    delay(time_step);
-//    int output_volt = analogRead(transducer1);
-//    Serial.print(millis());
-//    Serial.print(" ");
-//    Serial.print((float)i/255*5);
-//    Serial.print(" ");
-//    Serial.println((float)output_volt/1023*5.0);    
-//  }
-//  for(int i=99;i>=0;i--){
-//    analogWrite(2,i);
-//    delay(time_step);
-//    int output_volt = analogRead(transducer1);
-//    Serial.print(millis());
-//    Serial.print(" ");
-//    Serial.print((float)i/255*5);
-//    Serial.print(" ");
-//    Serial.println((float)output_volt/1023*5.0);
-//  }
-
-//  analogWrite(2,100);
-//  Serial.println(analogRead(input));
-void pressure_calibration(){
-  Serial.println("Set pressure to 0 and press enter");
+void run_pressure_calibration(){
+  Serial.println("Set pressure to 0 and press enter.");
   while (Serial.read() != 10);
   int v1 = (float)analogRead(transducer);
 
-  Serial.print("v1: ");
-  Serial.println(v1,DEC); 
+//   Serial.print("v1: ");
+//   Serial.println(v1,DEC); 
 
   Serial.println("Set pressure to 15 and press enter");
   while (Serial.read() != 10);
   int v2 = (float)analogRead(transducer);
   
-  Serial.print("v2: ");
-  Serial.println(v2,DEC); 
+//   Serial.print("v2: ");
+//   Serial.println(v2,DEC); 
 
   slope1 = (float) calibration_pressure/(v2-v1);
-  Serial.println("Slope: ");
+  Serial.print("slope1 = ");
   Serial.println(slope1,DEC); 
 
   int1 = (float)-v1*slope1;
-  Serial.print("Y-int: ");
-  Serial.println(int1,DEC); 
+  Serial.println("int1 = ");
+  Serial.println(int1,DEC);
 }
 
